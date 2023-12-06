349c39ca95efee2bd488cb170bbf82ad4aa52762
LKai-Xu <55689683+LKai-Xu@users.noreply.github.com>
2023-12-06 22:18:55 +0800

    Add files via upload

--- lab1/rtl/fused_signed_mac_32p8t8_2x24p8t4.v
+++ lab1/rtl/fused_signed_mac_32p8t8_2x24p8t4.v
@@ -7,17 +7,38 @@ module fused_signed_mac_32p8t8_2x24p8t4 (
     output [47:0] out
 );
 
-    // generate the bit-wise partial product
-    wire [7:0] partial_product [7:0];
-    generate
-        genvar i_pp;
-        for(i_pp=0;i_pp<8;i_pp=i_pp+1) begin: partial_product_generate
-            assign partial_product[i_pp] = a & b[i_pp];
-        end
-    endgenerate
+    wire [7:0] s_line [7:0];
+    wire [7:0] cout_line [7:0];
+    wire [7:0] s_line3_out;
+    wire [7:0] cout_line3_out;
+    wire [7:0] s_line4_in;
+    wire [7:0] cout_line4_in;
 
+    assign s_line4_in = split ? 8'b0 : s_line[3];
+    assign cout_line4_in = split ? 8'b0 : cout_line[3];
+
+    // array split low
+    basic_unit_first_line   i_bul_0     (.in_a(a), in_b(b[0]), .s(s_line[0]), .cout(cout_line[0]));
+    basic_unit_line         i_bul_1     (.in_x({1'b1, s_line[0][7:1]}), .in_a(a), in_b(b[1]), .cin(cout_line[0]), .s(s_line[1]), .cout(cout_line[1]));
+    basic_unit_line         i_bul_2     (.in_x({1'b0, s_line[1][7:1]}), .in_a(a), in_b(b[2]), .cin(cout_line[1]), .s(s_line[2]), .cout(cout_line[2]));
+    basic_unit_line         i_bul_3     (.in_x({1'b0, s_line[2][7:1]}), .in_a(a), in_b(b[3]), .cin(cout_line[2]), .s(s_line[3]), .cout(cout_line[3]));
+    basic_unit_last_line    i_bul_3l    (.in_x({1'b0, s_line[2][7:1]}), .in_a(a), in_b(b[3]), .cin(cout_line[2]), .s(s_line3_l), .cout(cout_line3_l));
+    // array split high
+    basic_unit_line         i_bul_4     (.in_x({1'b0, s_line4_in[7:1]}), .in_a(a), in_b(b[4]), .cin(cout_line4_in), .s(s_line[4]), .cout(cout_line[4]));
+    basic_unit_line         i_bul_5     (.in_x({1'b0, s_line[4][7:1]}), .in_a(a), in_b(b[5]), .cin(cout_line[4]), .s(s_line[5]), .cout(cout_line[5]));
+    basic_unit_line         i_bul_6     (.in_x({1'b0, s_line[5][7:1]}), .in_a(a), in_b(b[6]), .cin(cout_line[5]), .s(s_line[6]), .cout(cout_line[6]));
+    basic_unit_last_line    i_bul_7     (.in_x({1'b0, s_line[6][7:1]}), .in_a(a), in_b(b[7]), .cin(cout_line[6]), .s(s_line[7]), .cout(cout_line[7]));
+
+
+    // multiplication product
+    wire [15:0] out_8t8;
+    wire [11:0] out_8t4_low, out_8t4_high;
     // generate the 8t4 lower product
-
+    assign out_8t4_low[3:0] = s_line[3:0];
+    adder_9bit  i_adder_24p8t4_low      (.a({1'b1, s_line3_l}), .b(), .out());
+    // 24b8t4 output 
+    wire [31:0] out_32p8t8;
+    wire [23:0] out_24p8t4_low, out_24p8t4_high;
 
     // generate the 8t4 higher product
 
--- lab1/rtl/subckt.v
+++ lab1/rtl/subckt.v
@@ -1,4 +1,28 @@
+module fa(
+    input a,
+    input b,
+    input cin,
+    output s,
+    output cout
+);
 
+    assign s = a ^ b  ^ cin;
+    assign cout = (a & b) ^ (cin & (a ^ b));
+
+endmodule
+
+
+module ha(
+    input a,
+    input b,
+    output s,
+    output cout
+);
+
+    assign s = a ^ b;
+    assign cout = a & b;
+
+endmodule
 
 
 module basic_unit_ha_and(
@@ -73,22 +97,20 @@ endmodule
 
 
 module basic_unit_first_line(
-    input [7:0] in_x,
     input [7:0] in_a,
     input in_b,
-    input [7:0] cin,
     output [7:0] s,
     output [7:0] cout
 );
 
-    basic_unit_ha_and i_buha_0 (.in_x(in_x[0]), .in_a(in_a[0]), .in_b(in_b), .cin(cin[0]), .s(s[0]), .cout(cout[0]));
-    basic_unit_ha_and i_buha_1 (.in_x(in_x[1]), .in_a(in_a[1]), .in_b(in_b), .cin(cin[1]), .s(s[1]), .cout(cout[1]));
-    basic_unit_ha_and i_buha_2 (.in_x(in_x[2]), .in_a(in_a[2]), .in_b(in_b), .cin(cin[2]), .s(s[2]), .cout(cout[2]));
-    basic_unit_ha_and i_buha_3 (.in_x(in_x[3]), .in_a(in_a[3]), .in_b(in_b), .cin(cin[3]), .s(s[3]), .cout(cout[3]));
-    basic_unit_ha_and i_buha_4 (.in_x(in_x[4]), .in_a(in_a[4]), .in_b(in_b), .cin(cin[4]), .s(s[4]), .cout(cout[4]));
-    basic_unit_ha_and i_buha_5 (.in_x(in_x[5]), .in_a(in_a[5]), .in_b(in_b), .cin(cin[5]), .s(s[5]), .cout(cout[5]));
-    basic_unit_ha_and i_buha_6 (.in_x(in_x[6]), .in_a(in_a[6]), .in_b(in_b), .cin(cin[6]), .s(s[6]), .cout(cout[6]));
-    basic_unit_ha_nand i_buhna_7 (.in_x(in_x[7]), .in_a(in_a[7]), .in_b(in_b), .cin(cin[7]), .s(s[7]), .cout(cout[7]));
+    basic_unit_ha_and i_buha_0 (.in_x(1'b0), .in_a(in_a[0]), .in_b(in_b), .s(s[0]), .cout(cout[0]));
+    basic_unit_ha_and i_buha_1 (.in_x(1'b0), .in_a(in_a[1]), .in_b(in_b), .s(s[1]), .cout(cout[1]));
+    basic_unit_ha_and i_buha_2 (.in_x(1'b0), .in_a(in_a[2]), .in_b(in_b), .s(s[2]), .cout(cout[2]));
+    basic_unit_ha_and i_buha_3 (.in_x(1'b0), .in_a(in_a[3]), .in_b(in_b), .s(s[3]), .cout(cout[3]));
+    basic_unit_ha_and i_buha_4 (.in_x(1'b0), .in_a(in_a[4]), .in_b(in_b), .s(s[4]), .cout(cout[4]));
+    basic_unit_ha_and i_buha_5 (.in_x(1'b0), .in_a(in_a[5]), .in_b(in_b), .s(s[5]), .cout(cout[5]));
+    basic_unit_ha_and i_buha_6 (.in_x(1'b0), .in_a(in_a[6]), .in_b(in_b), .s(s[6]), .cout(cout[6]));
+    basic_unit_ha_nand i_buhna_7 (.in_x(1'b0), .in_a(in_a[7]), .in_b(in_b), .s(s[7]), .cout(cout[7]));
 
 
 endmodule
@@ -137,6 +159,29 @@ module basic_unit_last_line(
     basic_unit_fa_nand i_bufna_5 (.in_x(in_x[5]), .in_a(in_a[5]), .in_b(in_b), .cin(cin[5]), .s(s[5]), .cout(cout[5]));
     basic_unit_fa_nand i_bufna_6 (.in_x(in_x[6]), .in_a(in_a[6]), .in_b(in_b), .cin(cin[6]), .s(s[6]), .cout(cout[6]));
     basic_unit_fa_and i_bufa_7 (.in_x(in_x[7]), .in_a(in_a[7]), .in_b(in_b), .cin(cin[7]), .s(s[7]), .cout(cout[7]));
+
+
+endmodule
+
+
+module adder_9bit(
+    input   [8:0] a,
+    input   [8:1] b,
+    output  [8:0] out
+);
+
+    wire [8:0] s;
+    wire [8:0] cout;
+
+    ha i_ha_0 (.a(a[0]), .b(1'b0), .s(s[0]), .cout(cout[0]));
+    generate 
+        genvar i_adder;
+        for(i_adder=1; i_adder<9; i_adder=i_adder+1) begin: adder_for_bits
+            fa i_fa (.a(a[i_adder]), .b(b[i_adder]), .cin(cout[i_adder-1]), .s(s[i_adder]), .cout(cout[i_adder]));
+        end
+    endgenerate
+
+    assign out[8:0] = s[8:0];
 
 
 endmodule
