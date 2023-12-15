3bd484db9378ecd08ccafe949c5e0f6039930bc0
LKai-Xu <derek402@163.com>
2023-12-15 21:08:18 +0800

    initial

--- lab2/project/project.srcs/sources_1/imports/rtl/top_tb.v
+++ lab2/project/project.srcs/sources_1/imports/rtl/top_tb.v
@@ -24,11 +24,16 @@ module top_tb();
         start = 1'b0;
         #16 start = 1'b1;
         #10 start = 1'b0;
+        #174 start =1'b1;
+        #10 start=1'b0;
     end
 
     initial begin
-        // angle = 16'b0110010010000111;
+        // pai/6, sin=0.5, cos=(3/4)^0.5
         angle = 16'b0100001100000101;
+        #100
+        // pai/4, sin=(2/4)^0.5, cos=(2/4)^0.5
+        angle = 16'b0110010010000111;
     end
 
 
